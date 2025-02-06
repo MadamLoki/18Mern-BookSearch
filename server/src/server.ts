ca3ed79eee862d11dae73a30bf328f7c6ace59f3
Sara <lokifeathers@gmail.com>
2025-02-06 14:59:37 -0500

    Update server routes to serve production client files from the correct directory

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,12 +9,17 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/api', routes);
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  
+  // Serve index.html for any unknown paths 
+  app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
